445d2cad32fcf5fbeb1cb0ff98190f0a30d27b4b
宋徐林 <18702149475@163.com>
2026-05-28 19:41:54 +0800

    feat: add word flashcard image assets

---
+++ src/content/wordFlashcardImages.ts
@@ -0,0 +1,35 @@
+import bagImage from '../assets/word-flashcards/bag.png';
+import bedImage from '../assets/word-flashcards/bed.png';
+import bookImage from '../assets/word-flashcards/book.png';
+import boxImage from '../assets/word-flashcards/box.png';
+import chairImage from '../assets/word-flashcards/chair.png';
+import cupImage from '../assets/word-flashcards/cup.png';
+import doorImage from '../assets/word-flashcards/door.png';
+import friendImage from '../assets/word-flashcards/friend.png';
+import homeImage from '../assets/word-flashcards/home.png';
+import paperImage from '../assets/word-flashcards/paper.png';
+import penImage from '../assets/word-flashcards/pen.png';
+import phoneImage from '../assets/word-flashcards/phone.png';
+import roomImage from '../assets/word-flashcards/room.png';
+import studentImage from '../assets/word-flashcards/student.png';
+import tableImage from '../assets/word-flashcards/table.png';
+import windowImage from '../assets/word-flashcards/window.png';
+
+export const wordFlashcardImages: Partial<Record<string, string>> = {
+  bag: bagImage,
+  bed: bedImage,
+  book: bookImage,
+  box: boxImage,
+  chair: chairImage,
+  cup: cupImage,
+  door: doorImage,
+  friend: friendImage,
+  home: homeImage,
+  paper: paperImage,
+  pen: penImage,
+  phone: phoneImage,
+  room: roomImage,
+  student: studentImage,
+  table: tableImage,
+  window: windowImage,
+};
